81c2d3cedf1fe4cd01e5bf479e5a42e11ef24894
PranayPant <ppantg1@gmail.com>
2022-05-22 18:18:55 -0500

    added login functionality
    wip
    
    wip
    
    wip
    
    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-import Board from './components/Board'
+import Board from 'components/Board'
+import Login from 'components/Login'
+
+import {authenticateUser} from './api'
 
 function TicTacToe() {
+    const [token, setToken] = useState<string | null>(window.sessionStorage.getItem('access_token'))
+    const handleUserAuth = async (e: React.FormEvent<HTMLFormElement>, 
+        email: string) => {
+        e.preventDefault()
+        const res = await authenticateUser({email})
+        if(res.success){
+            window.sessionStorage.setItem('access_token', res.token)
+            setToken(res.token)
+        }
+    }
     return (
-        <div id="app">
+        <div id="app" className='centered'>
             <h1>Tic-Tac-Toe</h1>
-            <Board className="board" />
+            {token &&
+                <Board className="board" />
+            }
+            {!token &&
+                <Login handleSubmit={handleUserAuth} />
+            }
+            
         </div>
     )
 }
---
+++ src/types/UserAuth.ts
@@ -0,0 +1,8 @@
+export interface UserRequest {
+    email: string
+}
+
+export interface UserResponse {
+    success: boolean
+    token: string
+}
